40a1235bfec530a80181bbf2da2ab72a32f20921
Rajan14-11 <rajangoyal125@gmail.com>
2023-06-13 12:30:42 +0530

    new

--- src/components/Checkout/AddressForm.tsx
+++ src/components/Checkout/AddressForm.tsx
@@ -71,33 +71,33 @@ const AddressForm = (props: AddressFormProps) => {
   //   alert("Form Submitted")
   // };
 
-  useEffect(() => {
-    // console.log("addressCount", user.address);
-    if (submitFlag) {
-      // console.log("where are we", user);
-      let _address = {};
-      if (id) {
-        _address = {
-          _id: id,
-          name,
-          mobileNumber,
-          pinCode,
-          locality,
-          address,
-          cityDistrictTown,
-          state,
-          landmark,
-          alternatePhone,
-          addressType,
-        };
-      }
-      // else {
-      //   _address = user.address.slice(user.address.length - 1)[0];
-      // }
+  // useEffect(() => {
+  //   // console.log("addressCount", user.address);
+  //   if (submitFlag) {
+  //     // console.log("where are we", user);
+  //     let _address = {};
+  //     if (id) {
+  //       _address = {
+  //         _id: id,
+  //         name,
+  //         mobileNumber,
+  //         pinCode,
+  //         locality,
+  //         address,
+  //         cityDistrictTown,
+  //         state,
+  //         landmark,
+  //         alternatePhone,
+  //         addressType,
+  //       };
+  //     }
+  //     // else {
+  //     //   _address = user.address.slice(user.address.length - 1)[0];
+  //     // }
 
-      props.onSubmitForm(_address);
-    }
-  }, []);
+  //     props.onSubmitForm(_address);
+  //   }
+  // }, []);
 
   const renderAddressForm = () => {
     return (
