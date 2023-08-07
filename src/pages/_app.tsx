8fbd9e41c631bb046f8448ee50378d8d45d95da2
Rajan14-11 <rajangoyal125@gmail.com>
2023-08-07 15:12:35 +0530

    frontend compeletd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CartContextProvider } from "../Context/CartContext";
 import { Yeseva_One, Josefin_Sans } from "next/font/google";
-
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ContextProvider>
         <CartContextProvider>
+          <NextNProgress color="primary" />
           <Component {...pageProps} />
         </CartContextProvider>
       </ContextProvider>
